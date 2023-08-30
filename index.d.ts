f1983cdefd658b67c871b8ad2e95d353470f0bef
Camila Rondinini <crondinini@gmail.com>
2023-08-30 21:39:01 +0200

    fix error type

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import type {JsonObject} from 'type-fest';
 /**
 Exposed for `instanceof` checking.
 */
-export type JSONError = Error & { // eslint-disable-line @typescript-eslint/naming-convention
+export class JSONError extends Error { // eslint-disable-line @typescript-eslint/naming-convention
 	/**
 	The filename displayed in the error message, if any.
 	*/
@@ -13,7 +13,7 @@ export type JSONError = Error & { // eslint-disable-line @typescript-eslint/nami
 	The printable section of the JSON which produces the error.
 	*/
 	readonly codeFrame: string;
-};
+}
 
 // Get 'reviver' parameter from JSON.parse()
 type ReviverFn = Parameters<typeof JSON['parse']>['1'];
